a613ad2cbf64044aeebb718e2cf1758d93f205a0
juz41 <julek.zalewski@gmail.com>
2026-02-27 20:14:30 +0100

    test

--- main.py
+++
@@ -1,6 +0,0 @@
-def main():
-    print("Hello from wsi-proj1!")
-
-
-if __name__ == "__main__":
-    main()
--- regression_pre.py
+++ regression_pre.py
@@ -1,12 +1,14 @@
 #!/usr/bin/env python
 
 import pandas as pd
+
 data_path = "./data/regression"
 
 
 def check_df(df):
     cols_with_q = df.columns[df.isin(["?"]).any()]
     print(cols_with_q)
+
 
 df = pd.read_csv(f"{data_path}/dataset.csv")
 print(df.head())
@@ -18,15 +20,30 @@ for col in numeric_cols:
     df[col] = pd.to_numeric(df[col], errors="coerce")
     df[col] = df[col].fillna(df[col].mean())
 
-na_cols = ['Alley', 'BsmtQual', 'BsmtCond', 'Fence', 'PoolQC', 'BsmtExposure', 'BsmtFinType1', 'BsmtFinType2', 'FireplaceQu', 'GarageType', 'GarageFinish', 'GarageQual', 'GarageCond', 'MiscFeature']
+na_cols = [
+    "Alley",
+    "BsmtQual",
+    "BsmtCond",
+    "Fence",
+    "PoolQC",
+    "BsmtExposure",
+    "BsmtFinType1",
+    "BsmtFinType2",
+    "FireplaceQu",
+    "GarageType",
+    "GarageFinish",
+    "GarageQual",
+    "GarageCond",
+    "MiscFeature",
+]
 for col in na_cols:
     df[col] = df[col].replace("?", "NA")
 
-none_cols = ['MasVnrType']
+none_cols = ["MasVnrType"]
 for col in none_cols:
     df[col] = df[col].replace("?", "None")
 
-mix_cols = ['Electrical']
+mix_cols = ["Electrical"]
 for col in mix_cols:
     df[col] = df[col].replace("?", "Mix")
 
